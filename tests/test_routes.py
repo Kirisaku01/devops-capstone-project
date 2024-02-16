4f311cb7e88c6374ddc86312c71e63b0d4d30e06
Kirisaku01 <narabodeep@gmail.com>
2024-02-16 01:26:11 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,7 +141,7 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-        
+  
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -162,7 +162,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-    
+
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
